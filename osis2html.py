eaa60921c2cd2fdaa9ab8a7ae53da9d2b548e143
Scott Moonen <smoonen@us.ibm.com>
2022-09-28 07:15:43 -0700

    Accommodate latest jinja2

--- osis2html.py
+++ osis2html.py
@@ -214,7 +214,7 @@ for book in Books :
   book_title = book.node.getElementsByTagName('title')[0].firstChild.nodeValue
   variables = { 'book'       : book,
                 'book_title' : book_title,
-                'body'       : jinja2.Markup(Transformer().doc2html(book.node)) }
+                'body'       : jinja2.utils.markupsafe.Markup(Transformer().doc2html(book.node)) }
   bookFile.write(jenv.get_template('book.html').render(variables))
   bookFile.close()
 
